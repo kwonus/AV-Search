090d2672ea28f13d8bacc3a6d88b072385368a8f
Kevin Wonus <kevin@wonus.com>
2024-01-14 18:28:44 -0500

    added support for part of speech comparisons

--- Model/Features/FeaturePartOfSpeech.cs
+++ Model/Features/FeaturePartOfSpeech.cs
@@ -10,6 +10,14 @@ namespace AVSearch.Model.Features
 
         public override UInt16 Compare(AVXLib.Memory.Written writ, ref QueryMatch match, ref QueryTag tag)
         {
+            if (this.Pos32 != 0 && this.Pos32 == writ.POS32)
+            {
+                return FeatureGeneric.FullMatch;
+            }
+            if (this.PnPos12 != 0 && this.PnPos12 == (writ.POS32 & this.PnPos12))
+            {
+                return FeatureGeneric.FullMatch;
+            }
             return 0;
         }
         protected FeaturePartOfSpeech(string text, bool negate) : base(text, negate)
